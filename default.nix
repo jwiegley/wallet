ee661a6ae0c14fb81ed8ac84d4603304e3c7bc92
John Wiegley <johnw@newartisans.com>
2021-08-24 13:47:27 -0700

    Update quill and other tools

--- default.nix
+++ default.nix
@@ -12,14 +12,14 @@ rec {
 
 keysmith = with pkgs; buildGoModule rec {
   pname = "keysmith";
-  version = "3e2de90b";
+  version = "74c20ed5";
 
   src = fetchFromGitHub {
     owner = "dfinity";
     repo = "keysmith";
-    rev = "3e2de90bc268392b3000b45c307bf6a123ad04c0";
-    sha256 = "1z0sxirk71yabgilq8v5lz4nd2bbm1xyrd5zppif8k9jqhr6v3v3";
-    # date = 2021-07-02T02:56:38+02:00;
+    rev = "74c20ed5a8ae44214cb12abbfbe8f5887e2a4c33";
+    sha256 = "1mvqn2ir5nq5bh1h1aqjsv429m9gcfgky6mij5qnnjivp7i575mi";
+    # date = 2021-08-18T23:50:20+02:00;
   };
 
   vendorSha256 = "1p0r15ihmnmrybf12cycbav80sdj2dv2kry66f4hjfjn6k8zb0dc";
@@ -36,14 +36,14 @@ keysmith = with pkgs; buildGoModule rec {
 
 quill = with pkgs; rustPlatform.buildRustPackage rec {
   name = "quill-${version}";
-  version = "76dac5dd";
+  version = "7b2222eb";
 
   src = fetchFromGitHub {
     owner = "dfinity";
     repo = "quill";
-    rev = "76dac5ddb34f64aab3538ba3b5fa77b38a80ad3d";
-    sha256 = "0kyp2ji77a9faii9721k8972py0ac7p4ppqlklz5cikrxcyr6iab";
-    # date = 2021-07-14T08:44:25-07:00;
+    rev = "7b2222ebd32d1d4b4b9e67afb3c20849cb8f783b";
+    sha256 = "1lmh0s6qsjj2fn82k34bnyfz7y4w7fkvi1v00v8a9js9v60ma7md";
+    # date = 2021-08-23T20:32:01+02:00;
   };
 
   ic = fetchFromGitHub {
@@ -66,7 +66,7 @@ quill = with pkgs; rustPlatform.buildRustPackage rec {
     export OPENSSL_LIB_DIR=${openssl.out}/lib
   '';
 
-  cargoSha256 = "0clniq3whpcg3yx5kb2m81sbnng5z8337ahknz9cmh6qqqi3q3lf";
+  cargoSha256 = "0kp6niij4qync4s82f56c5njh1jszk2n6b462pljkvgig44nk4k5";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl protobuf ]
@@ -82,25 +82,37 @@ candid = with pkgs; rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "dfinity";
     repo = "candid";
-    rev = "0bbb320e66fddeccb674128640d1a3e6f697386a";
-    sha256 = "1i61dkzzw6c1g8ab09cpl5p3i4fkbd39zb87za7cqllyfi3iw0pm";
-    # date = 2021-07-27T14:05:09-07:00;
+    rev = "67e1e605aa772980560562b1d1b1af4b4afa02b2";
+    sha256 = "0y9d0ipy2w5a5nh4ynkmx1clqzg5vaq1zn21h05f4sf0b5yh2gj2";
+    # date = 2021-08-20T11:07:23-07:00;
   };
 
   registry = "file://local-registry";
 
-  # preBuild = ''
-  #   export REGISTRY_TRANSPORT_PROTO_INCLUDES=${ic}/rs/registry/transport/proto
-  #   export IC_BASE_TYPES_PROTO_INCLUDES=${ic}/rs/types/base_types/proto
-  #   export IC_PROTOBUF_PROTO_INCLUDES=${ic}/rs/protobuf/def
-  #   export IC_NNS_COMMON_PROTO_INCLUDES=${ic}/rs/nns/common/proto
+  cargoSha256 = "1inz1y1w3sbcvi7vjyapv2l9ag1wpvh2l2y5alfg59d9ff6k6gkp";
 
-  #   export PROTOC=${protobuf}/bin/protoc
-  #   export OPENSSL_DIR=${openssl.dev}
-  #   export OPENSSL_LIB_DIR=${openssl.out}/lib
-  # '';
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl protobuf ]
+    ++ lib.optionals stdenv.isDarwin [
+         libiconv darwin.apple_sdk.frameworks.Security
+       ];
+};
 
-  cargoSha256 = "1vbix102q8ajfb1driai24in2yc3mcrhipkrhjm0yfd2y7m1g5am";
+idl2json = with pkgs; rustPlatform.buildRustPackage rec {
+  name = "idl2json-${version}";
+  version = "21735b9c";
+
+  src = ~/dfinity/idl2json;
+  # src = fetchFromGitHub {
+  #   owner = "dfinity-lab";
+  #   repo = "idl2json";
+  #   rev = "bcb27ac567eba7a7c14c9d70aedd3fb777af281b";
+  #   sha256 = "0i61dkzzw6c1g8ab09cpl5p3i4fkbd39zb87za7cqllyfi3iw0pm";
+  # };
+
+  registry = "file://local-registry";
+
+  cargoSha256 = "1j9gvzcm0wmifv89lyga5x10cq0v3vrwr9dpr0713vx1x3fijks4";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl protobuf ]
@@ -115,8 +127,8 @@ dfx = pkgs.stdenv.mkDerivation rec {
 
   src = pkgs.fetchurl {
     url = "https://sdk.dfinity.org/install.sh";
-    sha256 = "0dqmv1rng3b8jf2gawdhqzl9h9am84kj6d52iyykyn2wzq8h12dg";
-    # date = 2021-07-28T08:48:30-0700;
+    sha256 = "1314krfh8drnm1j331glx8pkm81jgln278d5ngr0825ahqkzwy5y";
+    # date = 2021-07-30T22:18:57-0700;
   };
 
   buildInputs = with pkgs; [ curl cacert ];
@@ -134,7 +146,7 @@ dfx = pkgs.stdenv.mkDerivation rec {
 };
 
 shell = pkgs.mkShell {
-  buildInputs = [ keysmith quill candid dfx ];
+  buildInputs = [ keysmith quill candid idl2json dfx ];
 };
 
 }
